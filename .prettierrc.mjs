b6b4e1e4ce86b6a4b531f57344006b791d559cee
William Oldham <wegg7250@gmail.com>
2024-03-17 14:33:00 +0000

    More initial files

---
+++ .prettierrc.mjs
@@ -0,0 +1,15 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+  semi: false,
+  singleQuote: false,
+  trailingComma: "es5",
+}
