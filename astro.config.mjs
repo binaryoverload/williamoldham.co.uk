8ac099e65e9bc1adfc81ec8b5f97fb5f0b682cb0
William Oldham <wegg7250@gmail.com>
2024-03-17 21:33:06 +0000

    More config and styling and make all pages follow layouts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,20 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import emoji from "remark-emoji"
+import remarkMath from "remark-math"
+import rehypeMathjax from "rehype-mathjax"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [
-    mdx({
-      remarkPlugins: [[emoji, { accessible: true }]],
-      rehypePlugins: [],
-    }),
-    sitemap(),
-    tailwind(),
-  ],
+  markdown: {
+    remarkPlugins: [[emoji, { accessible: true }], remarkMath],
+    rehypePlugins: [rehypeMathjax],
+    gfm: true,
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "github-dark-default",
+    },
+  },
+  integrations: [mdx(), sitemap(), tailwind()],
 })
