d9911e754421b1b28d26d01a931b791a8d015115
William Oldham <github@binaryoverload.co.uk>
2025-09-27 19:38:41 +0100

    Add keywords metadata to BaseHead and update site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,16 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "William Oldham"
-export const SITE_DESCRIPTION = "Welcome to my website!"
+export const SITE_DESCRIPTION = "William Oldham's personal website and learnings as a systems engineer."
+export const SITE_KEYWORDS = [
+  "William Oldham",
+  "williamoldham",
+  "systems engineer",
+  "network engineer",
+  "software engineer",
+  "devops engineer",
+  "personal website",
+  "blog",
+  "notes",
+  "learnings",
+]
