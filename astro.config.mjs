aec8e74af7d0ff9253046b912f3769d23ef2c2ff
William Oldham <github@williamoldham.co.uk>
2026-02-08 20:33:13 +0000

    Upgrade tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig, envField } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 import emoji from "remark-emoji";
 import remarkMath from "remark-math";
 import rehypeMathjax from "rehype-mathjax";
@@ -13,15 +12,19 @@ import robotsTxt from "astro-robots-txt";
 
 import icon from "astro-icon";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://williamoldham.co.uk",
+
   env: {
     schema: {
       SHOW_FUTURE: envField.boolean({ context: 'server', access: 'public', default: false }),
       SHOW_DRAFTS: envField.boolean({ context: 'server', access: 'public', default: false }),
     }
   },
+
   markdown: {
     remarkPlugins: [[emoji, {
       accessible: true
@@ -55,5 +58,10 @@ export default defineConfig({
       theme: "github-dark-default"
     }
   },
-  integrations: [mdx(), sitemap(), tailwind(), robotsTxt(), icon()]
+
+  integrations: [mdx(), sitemap(), robotsTxt(), icon()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
